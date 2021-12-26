ff15cf4f59663d56635c32a486743ad00dc509b2
Uzeyr OZCAN <muzeyr@gmail.com>
2021-12-26 22:15:47 +0300

    access-control-allow-credentials

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,6 @@ const docs = process.env.SWAGGER || "docs";
 import swaggerDocument from "./config/swagger";
 import { connectMongoose } from "./config/db";
 import router from "./routes/routes";
-const cors = require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
